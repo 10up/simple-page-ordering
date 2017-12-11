fe016869d02ffad8be1e52f98b83896d778e2334
Ryan Welcher <ryan.welcher@10up.com>
2017-12-11 11:54:02 -0500

    Upading enqueue call to use new assets dir

--- simple-page-ordering.php
+++ simple-page-ordering.php
@@ -82,7 +82,7 @@ class Simple_Page_Ordering {
 	public static function wp() {
 		$orderby = get_query_var('orderby');
 		if ( ( is_string( $orderby ) && 0 === strpos( $orderby, 'menu_order' ) ) || ( isset( $orderby['menu_order'] ) && $orderby['menu_order'] == 'ASC' ) ) {
-			$script_name = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? 'simple-page-ordering.dev.js' : 'simple-page-ordering.js';
+			$script_name = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '/assets/js/src/simple-page-ordering.js' : '/assets/js/simple-page-ordering.min.js';
 			wp_enqueue_script( 'simple-page-ordering', plugins_url( $script_name, __FILE__ ), array('jquery-ui-sortable'), '2.1', true );
 			wp_enqueue_style( 'simple-page-ordering', plugins_url( 'simple-page-ordering.css', __FILE__ ) );
 		}
