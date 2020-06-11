a1c61197f35b5d9b549c877c04e2676c8ae6d230
Tung Du <dinhtungdu@gmail.com>
2020-06-11 16:55:36 +0700

    build: use webpack instead of grunt

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		'simple-page-ordering': './assets/js/src/simple-page-ordering.js',
+	},
+	output: {
+		filename: '[name].min.js',
+		path: path.resolve('./assets/js')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					}
+				},
+			},
+		],
+	}
+};
