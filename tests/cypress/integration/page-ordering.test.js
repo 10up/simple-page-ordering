88a6d513e536618dc0d42e8caf5a9381929230ef
Siddharth Thevaril <siddharth.thevaril@gmail.com>
2022-03-25 19:37:46 +0530

    add page ordering test

---
+++ tests/cypress/integration/page-ordering.test.js
@@ -0,0 +1,51 @@
+describe( 'Page ordering', () => {
+	before( () => {
+		cy.login( 'admin', 'password' );
+	} );
+
+	it( 'Page can be reordered', async () => {
+		cy.visit( `${ Cypress.config().baseUrl }wp-admin/edit.php?post_type=page` );
+
+		cy
+			.get( '.wp-list-table tbody tr:nth-child(1) .row-title' )
+			.then( $anchor => {
+				const pageTitle = $anchor.text();
+				cy.wrap( pageTitle ).as( 'firstPageTitle' );
+			} );
+
+
+		cy
+			.get( '.wp-list-table tbody tr:nth-child(2) .row-title' )
+			.then( $anchor => {
+				const pageTitle = $anchor.text();
+				cy.wrap( pageTitle ).as( 'secondPageTitle' );
+			} );
+
+
+		cy.window().then( win => {
+			const { jQuery } = win;
+
+			jQuery( '.wp-list-table tbody' ).sortable( 'option', 'update' )( null, {
+				item: jQuery( '.wp-list-table tbody tr:eq(0)' ).before(
+					jQuery( '.wp-list-table tbody tr:eq(1)' )
+				)
+			} );
+
+			cy
+				.get( '.wp-list-table tbody tr:nth-child(2) .row-title' )
+				.then( $anchor => {
+					cy.get( '@firstPageTitle' ).then( pageTitle => {
+						expect( $anchor.text() ).equals( pageTitle )
+					} );
+				} );
+
+			cy
+				.get( '.wp-list-table tbody tr:nth-child(1) .row-title' )
+				.then( $anchor => {
+					cy.get( '@secondPageTitle' ).then( pageTitle => {
+						expect( $anchor.text() ).equals( pageTitle )
+					} );
+				} );
+		} );
+	} );
+} );
