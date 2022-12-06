939e6688f81673e39e83dc00512ed38ace7324bb
JK <jayedulk@gmail.com>
2022-12-06 18:40:20 +0600

    Cypress integration migrated to 11+

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -25,7 +25,7 @@ module.exports = async (on, config) => {
     const port = wpEnvConfig.env.tests.port || null;
 
     if (port) {
-      config.baseUrl = wpEnvConfig.env.tests.config.WP_SITEURL;
+      // config.baseUrl = wpEnvConfig.env.tests.config.WP_SITEURL;
     }
   }
 
