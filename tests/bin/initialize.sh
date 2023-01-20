39d462ab2fe397eb5427e9cd63eecbb317bd3b29
JK <jayedulk@gmail.com>
2023-01-20 23:10:42 +0600

    E2E drag failing fix

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -5,8 +5,11 @@ npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
 # Create Posts
 npm run env run tests-cli "wp post create --post_status=publish --post_title='Post 1'"
 npm run env run tests-cli "wp post create --post_status=publish --post_title='Post 2'"
+npm run env run tests-cli "wp post create --post_status=publish --post_title='Post 3'"
 # Create Pages
 npm run env run tests-cli "wp post create --post_status=publish --post_type=page  --post_title='Page 1'"
 npm run env run tests-cli "wp post create --post_status=publish --post_type=page  --post_title='Page 2'"
+npm run env run tests-cli "wp post create --post_status=publish --post_type=page  --post_title='Page 3'"
 npm run env run tests-cli "wp post create --post_status=publish --post_type=page --post_parent=2 --post_title='Child Page 1'"
 npm run env run tests-cli "wp post create --post_status=publish --post_type=page --post_parent=2 --post_title='Child Page 2'"
+npm run env run tests-cli "wp post create --post_status=publish --post_type=page --post_parent=2 --post_title='Child Page 3'"
