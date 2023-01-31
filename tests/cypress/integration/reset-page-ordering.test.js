b3cf1c770d7b533098aa5c847b4b83eef6969eb5
ruscoe <danruscoe@protonmail.com>
2023-01-31 09:08:16 -0800

    Update reset page order test to replace cy.wait and to properly check original post titles after order reset.

--- tests/cypress/integration/reset-page-ordering.test.js
+++ tests/cypress/integration/reset-page-ordering.test.js
@@ -1,43 +1,45 @@
-describe('Test Reset Page Order Change', () => {
-	it('Can reset pages order', () => {
+describe( 'Test Reset Page Order Change', () => {
+	it( 'Can reset pages order', () => {
 		cy.login();
 		cy.visit('/wp-admin/edit.php?post_type=page');
 
-		const first = '.wp-list-table tbody tr:nth-child(1)';
-		const second = '.wp-list-table tbody tr:nth-child(2)';
-		const firstText = cy.get(`${first} .row-title`);
-		const secondText = cy.get(`${second} .row-title`);
+		const firstRow = '.wp-list-table tbody tr:nth-child(1)';
+		const secondRow = '.wp-list-table tbody tr:nth-child(2)';
 
-		// first reorder the pages.
-		cy.get(first).drag(second);
-		// wait for order update done.
-		cy.get(`${second}  .check-column input`).should('exist');
+		// Alias titles as `firstRowText` and `secondRowText` for convenience.
+		cy.get( firstRow ).find( '.row-title' ).invoke( 'text' ).as( 'firstRowText' );
+		cy.get( secondRow ).find( '.row-title' ).invoke( 'text' ).as( 'secondRowText' );
 
-		cy.get(`${first} .row-title`).then($el => {
-			secondText.should('have.text', $el.text());
-		});
-		cy.get(`${second} .row-title`).then($el => {
-			firstText.should('have.text', $el.text());
-		});
+		// Swap position of `Page 1` with `Page 2`.
+		cy.get( firstRow ).drag( secondRow );
 
-		// now reset the page order and verify original values are back.
-		cy.get(`#contextual-help-link`).click();
-		cy.get(`#tab-link-simple_page_ordering_help_tab`).click();
-		cy.get(`#simple-page-ordering-reset`).click();
-		cy.on(`window:confirm`, () => true);
+		// Verifies if 1st row has title `Page 2`.
+		cy.get( firstRow ).find( '.row-title' ).invoke( 'text' ).then( function( text ) {
+			expect( text ).to.eq( this.secondRowText );
+		} );
 
-		// wait for the page to reload before checking for reset post order.
-		cy.wait(1000);
+		// Verifies if 2nd row has title `Page 1`.
+		cy.get( secondRow ).find( '.row-title' ).invoke( 'text' ).then( function( text ) {
+			expect( text ).to.eq( this.firstRowText );
+		} );
 
-		// Re-query row elements that were detached from the DOM during the page reload.
-		const newFirstText = cy.get(`${first} .row-title`);
-		const newSecondText = cy.get(`${second} .row-title`);
+		// Now reset the page order and verify original values are back.
+		cy.get( '#contextual-help-link' ).click();
+		cy.get( '#tab-link-simple_page_ordering_help_tab' ).click();
+		cy.get( '#simple-page-ordering-reset' ).click();
+		cy.on( 'window:confirm', () => true );
 
-		cy.get(`${first} .row-title`).then($el => {
-			newFirstText.should('have.text', $el.text());
-		});
-		cy.get(`${second} .row-title`).then($el => {
-			newSecondText.should('have.text', $el.text());
-		});
-	});
-});
+		// Perform a reload as Cypress won't after window:confirm.
+		cy.reload();
+
+		// Verifies if 1st row has title `Page 1`.
+		cy.get( firstRow ).find( '.row-title' ).invoke( 'text' ).then( function( text ) {
+			expect( text ).to.eq( this.firstRowText );
+		} );
+
+		// Verifies if 2nd row has title `Page 2`.
+		cy.get( secondRow ).find( '.row-title' ).invoke( 'text' ).then( function( text ) {
+			expect( text ).to.eq( this.secondRowText );
+		} );
+	} );
+} );
