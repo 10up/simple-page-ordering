00c1d162d71c5679a4815aca5444e2e64e6fbd2a
Jeffrey Paul <jeffpaul@hotmail.com>
2026-04-15 21:07:13 -0500

    bump WP min to 6.8
    Co-Authored-By: Jasper <8646650+jasperfrontend@users.noreply.github.com>

--- simple-page-ordering.php
+++ simple-page-ordering.php
@@ -4,7 +4,7 @@
  * Plugin URI:        http://10up.com/plugins/simple-page-ordering-wordpress/
  * Description:       Order your pages and hierarchical post types using drag and drop on the built in page list. For further instructions, open the "Help" tab on the Pages screen.
  * Version:           2.7.4
- * Requires at least: 6.6
+ * Requires at least: 6.8
  * Requires PHP:      7.4
  * Author:            10up
  * Author URI:        https://10up.com
